ab8e6364e97fcddbfec79c5aa2cb8b4fd55f4dcc
karl anderson <karl@2600hz.com>
2014-03-12 14:35:35 -0700

    moving things around

--- rhel_install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-yum -y install python-devel python-pip
-pip install cython git+git://github.com/surfly/gevent.git#egg=gevent
---
+++ setup.py
@@ -0,0 +1,12 @@
+
+
+from distutils.core import setup
+
+setup(
+	name = 'rtmplite',
+	version = '9.1',
+	description = 'A python implementation of the Flash RTMP server with minimal support needed for real-time streaming and recording using AMF0',
+        url = 'https://code.google.com/p/rtmplite/',
+	license = 'GPL v3',
+	packages = ['rtmplite'],
+)
